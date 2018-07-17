5e2153f3966d8dd1ffd40dfd523b1855dd7cff7b
Abhi Sharma <abycoolnz05@gmail.com>
2018-07-17 13:12:58 +1200

    Add project files.

---
+++ SumOfOneToN/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SumOfOneToN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(175, 100);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(82, 13);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "Enter A Number";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(169, 145);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate Sum";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lblNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SumOfOneToN/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumOfOneToN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(txtNum.Text);
+            int sum = 0;
+
+            //for loop
+
+            //for (int N = 1; N < 100; N++)
+            //{
+            //    sum += i;
+         
+            //    MessageBox.Show(" The sum of 1 to N is: " + sum);               
+            //}
+
+            //do while loop
+
+            do
+            {
+                N++;
+                sum += N;
+                MessageBox.Show(" The Sum of 1 to N is: " + sum);
+
+            } while (N != 0);
+        }
+
+    }
+}
